2271d6d46300ff9571d2518a11e7de8053a286aa
Iulian-Tudor <itscutaru@gmail.com>
2023-12-05 21:05:22 +0200

    fixed database

--- EcryptedDatabase/database/database.py
+++ EcryptedDatabase/database/database.py
@@ -1,32 +1,38 @@
 import os
 from pymongo import MongoClient
 
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client['EncryptedDatabase']
+collection = db['file_collection']
 
-def add_file_to_database(ecrypted_file_path, symmetric_key, public_key, n):
-    with open(ecrypted_file_path, 'rb') as file:
-        file_data = file.read()
+def add_file_to_database(filename, public_key):
+    with open(filename, 'r') as file:
+        content = file.read()
 
-    encrypted_data = encrypt_message(file_data, symmetric_key, public_key, n)
+    encrypted_content = encrypt(content, public_key)
 
-    db.files.insert_one({'file_path': encrypted_file_path, 'encryption_method': 'encryption_method'})
+    with open(filename + '.enc', 'w') as file:
+        file.write(''.join(map(lambda x: str(x), encrypted_content)))
 
-
-
-
-def read_file_from_database(encrypted_file_path, symmetric_key, private_key, n):
-    file_info = db.files.find_one({'file_path': encrypted_file_path})
-
-    with open(encrypted_file_path, 'rb') as file:
-        encrypted_data = file.read()
-
-    decrypted_data = decrypt_message(encrypted_data, symmetric_key, private_key, n)
-
-    return decrypted_data
+    collection.insert_one({
+        'filename': filename,
+        'location': filename + '.enc'
+        'public_key': public_key
+    })
 
 
-def delete_from_database(encrypted_file_path):
-    db.files.delete_one({'file_path': encrypted_file_path})
+def get_file_from_database(filename, private_key):
+    file_data = collection.find_one({'filename': filename})
 
-    os.remove(encrypted_file_path)
+    with open(file_data['location'], 'r') as file:
+        encrypted_content = list(map(lambda x: int(x), file.readlines()))
+
+        content = decrypt(private_key, encrypted_content)
+        print(content)
+
+
+def delete_file(filename):
+    file_data = collection.find_one({'filename': filename})
+    os.remove(file_data['location'])
+    collection.delete_one({'filename': filename})
